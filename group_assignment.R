c9d9c8c2fa456bb8b98613a09fc46c4701276fc3
somahorvath113 <somahorvath113@gmail.com>
2025-10-31 23:33:10 +0100

    Add files via upload

--- group_assignment.R
+++ group_assignment.R
@@ -4,7 +4,7 @@ library(readxl)
 library(tidyverse)
 library(dplyr)
 library(tidyr)
-setwd("C:/Bator/1felev/Econometrics/empirical assignment")
+setwd("D://Egyetem//Econometrics//Group_assignment")
 data <- read_excel("data.xlsx")
 
 
@@ -89,15 +89,13 @@ cars_data <- cars_data %>% rename_with(~ new_names4)
 cars_data <- cars_data[,-c(1,3)]
 
 merged_data <- left_join(merged_data, cars_data, by = "county")
-merged_data$cars <- as.numeric() #budapest
+merged_data$cars <- as.numeric(merged_data$cars) #budapest
 merged_data$cars <- replace_na(merged_data$cars, 390.9)
-
 # creating highway dummy
 merged_data <- merged_data %>%
   mutate(highway = stringr::str_extract(address, "M\\d+"))
 merged_data <- merged_data %>%
   mutate(highway_dummy = ifelse(str_detect(address, "M\\d+"), 1, 0))
-
 #some statistics
 summary(merged_data)
 
@@ -127,11 +125,11 @@ gas1 <- lm(avg_gasoline ~ n_stations + region
            + highway_dummy + comp,  data = merged_data)
 summary(gas1)
 
-gas2<- lm(avg_gasoline ~ n_stations + Budapest
+gas2<- lm(avg_gasoline ~ pop + Budapest
           + highway_dummy + comp,  data = merged_data)
 summary(gas2)
 
-gas3<- lm(avg_gasoline ~ pop + areapstation + inc + cars +
+gas3<- lm(avg_gasoline ~ n_stations + areapstation + inc + cars +
             region + highway_dummy,  data = merged_data)
 summary(gas3)
 
@@ -149,6 +147,8 @@ summary(gas6)
 
 AIC(gas1, gas2, gas3, gas4, gas5, gas6)
 BIC(gas1, gas2, gas3, gas4, gas5, gas6) #gas2  a legjobb
+
+#regressions for diesel
 
 diesel1 <- lm(avg_diesel ~ n_stations + region
            + highway_dummy + comp,  data = merged_data)
@@ -179,8 +179,8 @@ library(skimr)
 #install.packages("gt")
 library(gt)
 vis_data <- merged_data
-vis_data <- vis_data[, c(2, 3, 6, 10, 11, 12 )]
-new_names5 <- c("Gasoline", "Diesel", "Number of stations", "Area", "Population", "Dwellings")
+vis_data <- vis_data[, c(2, 3, 6, 11, 26, 28, 29 )]
+new_names5 <- c("Gasoline", "Diesel", "Number of stations", "Population", "Area per Station", "Income", "Cars")
 vis_data <- vis_data %>% rename_with(~ new_names5)
 vis1 <- skim(vis_data)
 vis1 <- vis1[, -c(1,3, 4, 12)]
@@ -218,182 +218,109 @@ hist_diesel <- ggplot(merged_data, aes(x = avg_diesel)) +
   theme_minimal()
 hist_diesel
 
- hist_pop <- ggplot(merged_data, aes(x = pop)) +
-  
-  
+
+
+hist_pop <- ggplot(merged_data, aes(x = pop)) +
   geom_histogram(
-    
-    
     bins = 30,                
-    
-    
     fill = "steelblue",       
-    
-    
     color = "white",          
-    
-    
     alpha = 0.8               
-    
-    
   ) +
-  
-  
   labs(
-    
-    
     title = "Population of settlements in Hungary",
-    
-    
     x = "Population",
-    
-    
     y = "Frequency"
-    
-    
   ) +
-  
-  
   theme_minimal()
-
-
-
-
 
 hist_pop
 
-
-
-
-
 hist_dwe <- ggplot(merged_data, aes(x = dwellings)) +
-  
-  
   geom_histogram(
-    
-    
     bins = 30,                
-    
-    
     fill = "steelblue",       
-    
-    
     color = "white",          
-    
-    
     alpha = 0.8               
-    
-    
   ) +
-  
-  
   labs(
-    
-    
     title = "Number of Dwellings in Settlements in Hungary",
-    
-    
     x = "Number of Dwellings",
-    
-    
     y = "Frequency"
-    
-    
   ) +
-  
-  
   theme_minimal()
-
-
-
-
 
 hist_dwe
 
 
-
-
-
-
-
-
 gas_station_scat <- ggplot(merged_data, aes(x = dwellings, y = avg_gasoline)) +
-  
-  
   geom_point(
-    
-    
     color = "steelblue",  
-    
-    
     alpha = 0.7,          
-    
-    
     size = 3              
-    
-    
   ) +
-  
-  
   geom_smooth(
-    
-    
     method = "lm",        
-    
-    
     color = "darkred",    
-    
-    
     se = TRUE             
-    
-    
   ) +
-  
-  
   labs(
-    
-    
     title = "Scatterplot with Linear Regression Line",
-    
-    
     x = "Number of Dwellings",
-    
-    
     y = "Averge Price of Gasoline"
-    
-    
   ) +
-  
-  
   theme_minimal()
-
-
-
-
 
 gas_station_scat
 
 
 
+#visualization
+
+library(stargazer)
+
+aic_values <- c(AIC(gas1), AIC(gas2), AIC(gas5))
+bic_values <- c(BIC(gas1), BIC(gas2), BIC(gas5))
+
+extra_lines <- list(
+  c("AIC", sprintf("%.2f", aic_values)),
+  c("BIC", sprintf("%.2f", bic_values))
+)
 
 
 
+html_file <- "group_regression_table.html"
+sink(html_file)
+stargazer(
+  gas1, gas2, gas5,
+  type = "html",
+  title = "Regression Results",
+  add.lines = extra_lines,
+  digits = 2
+)
+sink()
+
+webshot(html_file, file = "groupregression_table.png", zoom = 2, vwidth = 1200)
 
 
-gas5 <- lm(avg_gasoline ~ n_stations + cars  + region + highway_dummy 
-           
-           
-           + comp,  data = merged_data)
+aic_values2 <- c(AIC(diesel1), AIC(diesel2), AIC(diesel3))
+bic_values2 <- c(BIC(diesel1), BIC(diesel2), BIC(diesel3))
 
 
-summary(gas5)
+extra_lines2 <- list(
+  c("AIC", sprintf("%.2f", aic_values2)),
+  c("BIC", sprintf("%.2f", bic_values2))
+)
 
+html_file2 <- "group_regression_table2.html"
+sink(html_file2)
+stargazer(
+  diesel1, diesel2, diesel3,
+  type = "html",
+  title = "Regression Results",
+  add.lines = extra_lines2,
+  digits = 2
+)
+sink()
 
-
-diesel5 <- lm(avg_diesel ~   inc  + region + highway_dummy 
-              
-              
-              + comp + n_stations,  data = merged_data)
-
-
-summary(diesel5)
+webshot(html_file2, file = "groupregression_table2.png", zoom = 2, vwidth = 1200)
