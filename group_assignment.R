bab50bd3638bdb43ba175f6e3648a8213a5bf60f
BatorBirovecz <bator.birovecz@stud.uni-corvinus.hu>
2025-10-31 17:10:14 +0100

    Add files via upload

--- group_assignment.R
+++ group_assignment.R
@@ -4,7 +4,7 @@ library(readxl)
 library(tidyverse)
 library(dplyr)
 library(tidyr)
-setwd("D://Egyetem//Econometrics//Group_assignment")
+setwd("C:/Bator/1felev/Econometrics/empirical assignment")
 data <- read_excel("data.xlsx")
 
 
@@ -88,11 +88,9 @@ new_names4 <- c("code", "county", "region", "cars")
 cars_data <- cars_data %>% rename_with(~ new_names4)
 cars_data <- cars_data[,-c(1,3)]
 
-merged_data <- inner_join(merged_data, cars_data, by = "county")
-
-#do it numeric
-is.numeric(merged_data$cars)
-merged_data$cars <- as.numeric(merged_data$cars)
+merged_data <- left_join(merged_data, cars_data, by = "county")
+merged_data$cars <- as.numeric() #budapest
+merged_data$cars <- replace_na(merged_data$cars, 390.9)
 
 # creating highway dummy
 merged_data <- merged_data %>%
@@ -109,180 +107,293 @@ merged_data$city <- as.factor(merged_data$city)
 merged_data$county <- as.factor(merged_data$county)
 merged_data$dist_seat <- as.factor(merged_data$dist_seat)
 merged_data$legal_stat <- as.factor(merged_data$legal_stat)
+merged_data$region <- as.factor(merged_data$region)
+merged_data$region <- relevel(merged_data$region, ref = "Budapest")
+merged_data <- merged_data %>%
+  mutate(Budapest = fct_other(region, keep = "Budapest", other_level = "other"))
+merged_data$Budapest <- relevel(merged_data$Budapest, ref = "other")
 #merged_data[,13-25] <- replace_na(merged_data[,13-25], 0)
 
-#regressions for gasoline
-gas1 <- lm(avg_gasoline ~ avg_diesel, data = merged_data)
-summary(gas1)
-
-gas2<- lm(avg_gasoline ~ avg_diesel  + dwellings + pop + areapstation + inc + cars,
-             data = merged_data)
-summary(gas2)
-
-gas3<- lm(avg_gasoline ~ dwellings + pop + areapstation + inc + cars +
-            region + highway_dummy,  data = merged_data)
-summary(gas3)
-
-#just the 4 biggest company stays
+#just the 4 biggest company needed
 library(forcats)
 keep <- c("Mol","Shell","Omv","Orlen")   
 merged_data <- merged_data %>%
   mutate(comp = fct_other(comp, keep = keep, other_level = "other"))
 merged_data$comp <- relevel(merged_data$comp, ref = "other")
 
-gas4 <- lm(avg_gasoline ~ pop + inc + cars + region + highway_dummy 
-           + n_stations + comp,  data = merged_data)
+
+#regressions for gasoline
+gas1 <- lm(avg_gasoline ~ n_stations + region
+           + highway_dummy + comp,  data = merged_data)
+summary(gas1)
+
+gas2<- lm(avg_gasoline ~ n_stations + Budapest
+          + highway_dummy + comp,  data = merged_data)
+summary(gas2)
+
+gas3<- lm(avg_gasoline ~ pop + areapstation + inc + cars +
+            region + highway_dummy,  data = merged_data)
+summary(gas3)
+
+gas4 <- lm(avg_gasoline ~ pop + areapstation +inc + cars + region
+           + highway_dummy + comp,  data = merged_data)
 summary(gas4)
 
-library(corrplot)
+gas5 <- lm(avg_gasoline ~ n_stations + areapstation +inc + cars + region
+           + highway_dummy + comp,  data = merged_data)
+summary(gas5)
+
+gas6 <- lm(avg_gasoline ~ n_stations + areapstation +inc + cars + region
+           + highway_dummy + comp + legal_stat,  data = merged_data)
+summary(gas6)
+
+AIC(gas1, gas2, gas3, gas4, gas5, gas6)
+BIC(gas1, gas2, gas3, gas4, gas5, gas6) #gas2  a legjobb
+
+diesel1 <- lm(avg_diesel ~ n_stations + region
+           + highway_dummy + comp,  data = merged_data)
+summary(diesel1)
+
+diesel2 <- lm(avg_diesel ~ pop + region
+              + highway_dummy + comp,  data = merged_data)
+summary(diesel2)
+
+diesel3 <- lm(avg_diesel ~ n_stations + Budapest
+              + highway_dummy + comp,  data = merged_data)
+summary(diesel3)
+
+AIC(diesel1, diesel2, diesel3)
+BIC(diesel1, diesel2, diesel3) #diesel 3 - ugyanaz mint gas2
 
 
+#correlaton matrix
 selected_cols <- c("avg_gasoline", "avg_diesel", "n_stations", "pop", "dwellings", "areapstation", "inc", "cars", "highway_dummy")
 corrplot_data <- merged_data[selected_cols]
-
 cor_matrix <- cor(corrplot_data, use = "pairwise.complete.obs")
-
 corrplot::corrplot(cor_matrix, method = "number", type = "full")
-
-
-
+#pop, dwellings, n_station -> just one of them
 
 # descriptive statistics
-
+#install.packages("skimr")
 library(skimr)
+#install.packages("gt")
 library(gt)
-
 vis_data <- merged_data
-
 vis_data <- vis_data[, c(2, 3, 6, 10, 11, 12 )]
-
 new_names5 <- c("Gasoline", "Diesel", "Number of stations", "Area", "Population", "Dwellings")
 vis_data <- vis_data %>% rename_with(~ new_names5)
-
-
 vis1 <- skim(vis_data)
 vis1 <- vis1[, -c(1,3, 4, 12)]
 
 # visulalisation 
-
 gt_table <- vis1 %>%
   gt() %>%
-  tab_header(
-    title = "Descriptive Statistics"
-  ) %>%
+  tab_header(title = "Descriptive Statistics") %>%
   fmt_number(
     columns = vars(numeric.mean, numeric.sd, numeric.p0, numeric.p25, numeric.p50, numeric.p75, numeric.p100),
-    decimals = 2
-  ) %>%
-  cols_label(
-    skim_variable = "Variable",
+    decimals = 2) %>%
+  cols_label(skim_variable = "Variable",
     numeric.mean = "Average",
     numeric.sd = "Standard deviation",
     numeric.p0 = "Min",
-    numeric.p25 = "25. Percentile",
-    numeric.p50 = "Median",
+    numeric.p25 = "25. Percentile", numeric.p50 = "Median",
     numeric.p75 = "75. Percentile",
-    numeric.p100 = "Max"
-  ) %>%
-  tab_options(
-    table.font.names = "Arial",
-    table.background.color = "white"
-  )
+    numeric.p100 = "Max") %>%
+  tab_options(table.font.names = "Arial",
+    table.background.color = "white")
 print(gt_table)
 
 library(ggplot2)
-
 hist_gas <- ggplot(merged_data, aes(x = avg_gasoline)) +
-  geom_histogram(
-    bins = 30,                
-    fill = "steelblue",       
-    color = "white",          
-    alpha = 0.8               
-  ) +
-  labs(
-    title = "Averge Price of Gasoline in Hungary",
+  geom_histogram(binwidth = 5, fill = "steelblue", color = "white",alpha = 0.8)+              
+  labs(title = "Averge Price of Gasoline in Hungary",
     x = "Average price of gasoline",
-    y = "Frequency"
-  ) +
-  theme_minimal()
-
+    y = "Frequency") +
+   theme_minimal()
 hist_gas
-
-
 hist_diesel <- ggplot(merged_data, aes(x = avg_diesel)) +
-  geom_histogram(
-    bins = 30,                
-    fill = "steelblue",       
-    color = "white",          
-    alpha = 0.8               
-  ) +
-  labs(
-    title = "Averge Price of Diesel in Hungary",
-    x = "Average price of diesel",
-    y = "Frequency"
-  ) +
+  geom_histogram(binwidth = 5, fill = "steelblue", color = "white", alpha = 0.8) +
+  labs(title = "Averge Price of Diesel in Hungary",
+       x = "Average price of diesel", y = "Frequency") +
   theme_minimal()
-
 hist_diesel
 
-hist_pop <- ggplot(merged_data, aes(x = pop)) +
+ hist_pop <- ggplot(merged_data, aes(x = pop)) +
+  
+  
   geom_histogram(
+    
+    
     bins = 30,                
+    
+    
     fill = "steelblue",       
+    
+    
     color = "white",          
+    
+    
     alpha = 0.8               
+    
+    
   ) +
+  
+  
   labs(
+    
+    
     title = "Population of settlements in Hungary",
+    
+    
     x = "Population",
+    
+    
     y = "Frequency"
+    
+    
   ) +
+  
+  
   theme_minimal()
+
+
+
+
 
 hist_pop
 
+
+
+
+
 hist_dwe <- ggplot(merged_data, aes(x = dwellings)) +
+  
+  
   geom_histogram(
+    
+    
     bins = 30,                
+    
+    
     fill = "steelblue",       
+    
+    
     color = "white",          
+    
+    
     alpha = 0.8               
+    
+    
   ) +
+  
+  
   labs(
+    
+    
     title = "Number of Dwellings in Settlements in Hungary",
+    
+    
     x = "Number of Dwellings",
+    
+    
     y = "Frequency"
+    
+    
   ) +
+  
+  
   theme_minimal()
+
+
+
+
 
 hist_dwe
 
 
+
+
+
+
+
+
 gas_station_scat <- ggplot(merged_data, aes(x = dwellings, y = avg_gasoline)) +
+  
+  
   geom_point(
+    
+    
     color = "steelblue",  
+    
+    
     alpha = 0.7,          
+    
+    
     size = 3              
+    
+    
   ) +
+  
+  
   geom_smooth(
+    
+    
     method = "lm",        
+    
+    
     color = "darkred",    
+    
+    
     se = TRUE             
+    
+    
   ) +
+  
+  
   labs(
+    
+    
     title = "Scatterplot with Linear Regression Line",
+    
+    
     x = "Number of Dwellings",
+    
+    
     y = "Averge Price of Gasoline"
+    
+    
   ) +
+  
+  
   theme_minimal()
+
+
+
+
 
 gas_station_scat
 
 
+
+
+
+
+
+
 gas5 <- lm(avg_gasoline ~ n_stations + cars  + region + highway_dummy 
-            + comp,  data = merged_data)
+           
+           
+           + comp,  data = merged_data)
+
+
 summary(gas5)
 
+
+
 diesel5 <- lm(avg_diesel ~   inc  + region + highway_dummy 
-           + comp + n_stations,  data = merged_data)
+              
+              
+              + comp + n_stations,  data = merged_data)
+
+
 summary(diesel5)
